a48d01537ab0f8e578258a7620e569ef57384822
Manoel Lobo <manoel.lobo@gmail.com>
2018-09-05 09:05:12 -0300

    Basic styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Main from './pages/Main';
+import './styles/global';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => <Main />;
 
 export default App;
